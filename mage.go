947a5cbb2891f641c6d872e6ac092f34cf8ce136
Jack Lindamood <jack@cresta.ai>
2022-02-09 19:52:51 +0000

    New PR

---
+++ mage.go
@@ -0,0 +1,21 @@
+//go:build mage
+// +build mage
+
+package main
+
+import (
+	_ "github.com/cresta/magehelper/cicd/githubactions"
+	"github.com/cresta/magehelper/docker/registry"
+
+	// mage:import go
+	_ "github.com/cresta/magehelper/gobuild"
+	// mage:import docker
+	_ "github.com/cresta/magehelper/docker"
+	// mage:import ghcr
+	"github.com/cresta/magehelper/docker/registry/ghcr"
+)
+
+func init() {
+	// Install ECR as my registry
+	registry.Instance = ghcr.Instance
+}
